5c361aea4c5e024cd298823bc6145c0334099a5b
D.J. O'Leary <djoleary39@ymail.com>
2020-12-24 00:54:16 +0000

    Moved functions around

--- src/Collider/Collider.js
+++ src/Collider/Collider.js
@@ -1,82 +1,82 @@
 class Collider {
-  /**
-   * The default constructor of the class.
-   * @param {Shape} shape 
-   * @param {String[]} objectTags
-   * @param {String[]} ignoreTags
-   */
-  constructor(shape, objectTags, ignoreTags) {
-    this.colliding = false;
-    this.checkedForCollision = false;
-    this.shape = shape;
-    this.tags = new ColliderTags(objectTags, ignoreTags);
-    this.screenPos = new Vector2(0, 0);
-  }
+    /**
+     * The default constructor of the class.
+     * @param {Shape} shape 
+     * @param {String[]} objectTags
+     * @param {String[]} ignoreTags
+     */
+    constructor(shape, objectTags, ignoreTags) {
+        this.colliding = false;
+        this.checkedForCollision = false;
+        this.shape = shape;
+        this.tags = new ColliderTags(objectTags, ignoreTags);
+        this.screenPos = new Vector2(0, 0);
+    }
 
-  get objectTags() {
-    return this.tags.objectTags;
-  }
+    /**
+     * 
+     * @param {String} tag 
+     * @return {Boolean}
+     */
+    hasObjectTag(tag) {
+        return this.tags.hasObjectTag(tag);
+    }
 
-  get ignoreTags() {
-    return this.tags.ignoreTags;
-  }
+    /**
+     * 
+     * @param {String} tag 
+     * @return {Boolean}
+     */
+    hasIgnoreTag(tag) {
+        return this.tags.hasIgnoreTag(tag);
+    }
 
-  /**
-   * 
-   * @param {String} tag 
-   * @return {Boolean}
-   */
-  containsObjectTag(tag) {
-    return this.tags.hasObjectTag(tag);
-  }
+    /**
+     * 
+     * @param {Scalar} gridWidth 
+     * @param {Scalar} gridHeight 
+     */
+    updateSpatialHash(gridWidth, gridHeight) {
+        this.screenPos = new Vector2(Math.floor(this.position.x / gridWidth), Math.floor(this.position.y / gridHeight));
+    }
 
-  /**
-   * 
-   * @param {String} tag 
-   * @return {Boolean}
-   */
-  containsIgnoreTag(tag) {
-    return this.tags.hasIgnoreTag(tag);
-  }
+    /**
+     * 
+     * @param {Integer} x 
+     * @param {Integer} y 
+     */
+    move(x, y) {
+        this.shape.move(x, y);
+    }
 
-  /**
-   * 
-   * @param {Scalar} gridWidth 
-   * @param {Scalar} gridHeight 
-   */
-  updateSpatialHash(gridWidth, gridHeight) {
-    this.screenPos = new Vector2(Math.floor(this.position.x / gridWidth), Math.floor(this.position.y / gridHeight));
-  }
+    /**
+     * 
+     * @param {Float} scale 
+     */
+    scale(scale) {
+        this.shape.scale(scale);
+    }
 
-  /**
-   * 
-   */
-  get position() {
-    return this.shape.position;
-  }
+    /**
+     * 
+     */
+    get position() {
+        return this.shape.position;
+    }
 
-  /**
-   * 
-   * @param {Vector2} newPos
-   */
-  set position(newPos) {
-    this.shape.position = newPos;
-  }
+    /**
+     * 
+     * @param {Vector2} newPos
+     */
+    set position(newPos) {
+        this.shape.position = newPos;
+    }
 
-  /**
-   * 
-   * @param {Integer} x 
-   * @param {Integer} y 
-   */
-  move(x, y) {
-    this.shape.move(x, y);
-  }
+    get objectTags() {
+        return this.tags.objectTags;
+    }
 
-  /**
-   * 
-   * @param {Float} scale 
-   */
-  scale(scale) {
-    this.shape.scale(scale);
-  }
+    get ignoreTags() {
+        return this.tags.ignoreTags;
+    }
 }
