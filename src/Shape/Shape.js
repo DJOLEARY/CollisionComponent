66778e7cb45413982f83bafa6f7f38d2bfcd976d
D.J. O'Leary <djoleary39@ymail.com>
2020-12-26 17:35:59 +0000

    Added abstract functions for move, scale, and rotate

--- src/Shape/Shape.js
+++ src/Shape/Shape.js
@@ -3,4 +3,16 @@ class Shape {
     constructor(position) {
         this.position = position
     }
+
+    move(x, y) {
+        throw new AbstractFunctionError()
+    }
+
+    scale(ratio){
+        throw new AbstractFunctionError()
+    }
+
+    rotate(degrees){
+        throw new AbstractFunctionError()
+    }
 }
