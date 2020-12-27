ce3dbfa2ccf2acf77f3891a6d23b003943c750d0
D.J. O'Leary <djoleary39@ymail.com>
2020-12-27 20:52:32 +0000

    Added extra functionality

--- src/Vector2.js
+++ src/Vector2.js
@@ -1,5 +1,9 @@
 class Vector2 {
 
+    static FromArray(array) {
+        return new Vector2(array[0], array[1])
+    }
+
     constructor(x, y) {
         this.x = x
         this.y = y
@@ -9,15 +13,27 @@ class Vector2 {
         return (this.x * otherVector.x) + (this.y * otherVector.y)
     }
 
-    perp() {
-        return new Vector2(-this.y, this.x)
-    }
-
     add(otherVector) {
         return new Vector2(this.x + otherVector.x, this.y + otherVector.y)
     }
 
     subtract(otherVector) {
         return new Vector2(this.x - otherVector.x, this.y - otherVector.y)
+    }
+
+    isEqual(otherVector) {
+        return this.x === otherVector.x && this.y === otherVector.y
+    }
+
+    asArray() {
+        return [this.x, this.y]
+    }
+
+    get unit() {
+        return MathHelper.Normalise(this)        
+    }
+
+    get normal() {
+        return new Vector2(-this.y, this.x)
     }
 }
