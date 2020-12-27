8a2a4c4db63c802ef95477847e65312ef90ef567
D.J. O'Leary <djoleary39@ymail.com>
2020-12-27 00:36:36 +0000

    Updated tests

--- test/CollisionComponent.js
+++ test/CollisionComponent.js
@@ -1,60 +1,60 @@
 /* global CollisionComponent, describe, it, expect, should */
 
 describe('CollisionManager()', function () {
-  'use strict'
+    'use strict'
 
-  it('AxisAlignedBoundingBox exists', function () {
-    expect(CollisionManager.AxisAlignedBoundingBox).to.be.a('function')
-  })
+    it('Test AABB - Collision', function () {
+        var rect1 = new Rectangle(new Vector2(0, 0), 2, 2)
+        var rect2 = new Rectangle(new Vector2(0, 0), 2, 2)
 
-  it('CircleCollision exists', function () {
-    expect(CollisionManager.CircleCollision).to.be.a('function')
-  })
+        var collisionTheorem = new AxisAlignedBoundingBox(rect1, rect2)
+        var result = collisionTheorem.testForCollision()
+        expect(result).to.equal(true)
+    })
 
-  it('SeperatingAxisTheorem exists', function () {
-    expect(CollisionManager.SeperatingAxisTheorem).to.be.a('function')
-  })
+    it('Test AABB - No Collision', function () {
+        var rect1 = new Rectangle(new Vector2(0, 0), 2, 2)
+        var rect2 = new Rectangle(new Vector2(5, 0), 2, 2)
 
-  it('Test AABB - Collision', function () {
-    var rect1 = new BoxCollider(new Vector2(0,0), 2, 2)
-    var rect2 = new BoxCollider(new Vector2(0,0), 2, 2)
+        var collisionTheorem = new AxisAlignedBoundingBox(rect1, rect2)
+        var result = collisionTheorem.testForCollision()
+        expect(result).to.equal(false)
+    })
 
-    expect(CollisionManager.AxisAlignedBoundingBox(rect1, rect2)).to.equal(true)
-  })
+    it('Test CircleCollision - Collision', function () {
+        var circle1 = new Circle(new Vector2(0, 0), 2)
+        var circle2 = new Circle(new Vector2(1, 0), 2)
 
-  it('Test AABB - No Collision', function () {
-    var rect1 = new BoxCollider(new Vector2(0,0), 2, 2)
-    var rect2 = new BoxCollider(new Vector2(5,0), 2, 2)
+        var collisionTheorem = new CircleCollision(circle1, circle2)
+        var result = collisionTheorem.testForCollision()
+        expect(result).to.equal(true)
+    })
 
-    expect(CollisionManager.AxisAlignedBoundingBox(rect1, rect2)).to.equal(false)
-  })
+    it('Test CircleCollision - No Collision', function () {
+        var circle1 = new Circle(new Vector2(0, 0), 2)
+        var circle2 = new Circle(new Vector2(5, 0), 2)
 
-  it('Test CircleCollision - Collision', function () {
-    var circle1 = new CircleCollider(new Vector2(0,0), 2)
-    var circle2 = new CircleCollider(new Vector2(1,0), 2)
+        var collisionTheorem = new CircleCollision(circle1, circle2)
+        var result = collisionTheorem.testForCollision()
+        expect(result).to.equal(false)
+    })
 
-    expect(CollisionManager.CircleCollision(circle1, circle2)).to.equal(true)
-  })
+    it('Test SAT - Collision', function () {
+        var polygon1 = new Polygon([new Vector2(0, 0), new Vector2(2, 0), new Vector2(2, -2), new Vector2(0, -2)])
+        var polygon2 = new Polygon([new Vector2(1, 0), new Vector2(3, 0), new Vector2(3, -2), new Vector2(1, -2)])
 
-  it('Test CircleCollision - No Collision', function () {
-    var circle1 = new CircleCollider(new Vector2(0,0), 2)
-    var circle2 = new CircleCollider(new Vector2(5,0), 2)
+        var collisionTheorem = new SeperatingAxisTheorem(polygon1, polygon2)
+        var result = collisionTheorem.testForCollision()
+        expect(result).to.equal(true)
+    })
 
-    expect(CollisionManager.CircleCollision(circle1, circle2)).to.equal(false)
-  })
+    it('Test SAT - No Collision', function () {
+        var polygon1 = new Polygon([new Vector2(0, 0), new Vector2(2, 0), new Vector2(2, -2), new Vector2(0, -2)])
+        var polygon2 = new Polygon([new Vector2(3, 0), new Vector2(5, 0), new Vector2(5, -2), new Vector2(3, -2)])
 
-  it('Test SAT - Collision', function () {
-    var polygon1 = new PolygonCollider([new Vector2(0,0), new Vector2(2, 0), new Vector2(2, -2), new Vector2(0, -2)])
-    var polygon2 = new PolygonCollider([new Vector2(1,0), new Vector2(3, 0), new Vector2(3, -2), new Vector2(1, -2)])
-
-    expect(CollisionManager.SeperatingAxisTheorem(polygon1, polygon2)).to.equal(true)
-  })
-
-  it('Test SAT - No Collision', function () {
-    var polygon1 = new PolygonCollider([new Vector2(0,0), new Vector2(2, 0), new Vector2(2, -2), new Vector2(0, -2)])
-    var polygon2 = new PolygonCollider([new Vector2(3,0), new Vector2(5, 0), new Vector2(5, -2), new Vector2(3, -2)])
-
-    expect(CollisionManager.SeperatingAxisTheorem(polygon1, polygon2)).to.equal(false)
-  })
+        var collisionTheorem = new SeperatingAxisTheorem(polygon1, polygon2)
+        var result = collisionTheorem.testForCollision()
+        expect(result).to.equal(false)
+    })
 
 })
